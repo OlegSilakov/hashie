abd52dbba669a0a353be562430a84c80734afccf
Bebop <bebop@bebop-SATELLITE-L755.(none)>
2014-06-19 11:27:16 +0400

    mash start implementation

--- lib/hashie.rb
+++ lib/hashie.rb
@@ -3,5 +3,6 @@ require "coveralls"
 
 module Hashie
 	Coveralls.wear!
+	autoload :Mash, 'hashie/mash'
   # Your code goes here...
 end
---
+++ lib/hashie/mash.rb
@@ -0,0 +1,7 @@
+class Mash < Hash
+
+	def method_missing(meth, *args, &block)
+		false
+	end
+
+end
